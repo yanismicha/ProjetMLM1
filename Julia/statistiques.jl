20af60ab0f8c6cf1cbdb3de2461038e8daf6ef66
Micha Yanis <yanismicha02@gmail.com>
2023-11-06 14:25:25 +0100

    visualisation data

--- Julia/statistiques.jl
+++ Julia/statistiques.jl
@@ -1,5 +1,5 @@
-using CSV,Downloads,Plots,DataFrames
-dtFinal=CSV.File(Downloads.download("https://www.dropbox.com/scl/fi/5gdv6tjn0ojbl1q9d354e/membersClean.csv?rlkey=3x35c1wp23774vk0vr0iygp6c&dl=0")) |> DataFrame
+using CSV,Downloads,DataFrames
+dtFinal=CSV.File(Downloads.download("https://www.dropbox.com/scl/fi/d3v41yp6x9cxlqvueoet3/membersClean.csv?rlkey=v9xfdgu6oyubjur9rlu6k9eow&dl=0"),normalizenames = true) |> DataFrame
 
 
 
@@ -7,31 +7,13 @@ dtFinal=CSV.File(Downloads.download("https://www.dropbox.com/scl/fi/5gdv6tjn0ojb
 counts = combine(groupby(dtFinal, :peak_name), nrow)  #permet d'avoir le nombre de valeurs prises pour chaque modalités
 counts=sort(counts,[order(:nrow)]) #je l'odronne pour que ce soit plus lisible
 using Plots
-bar(counts.peak_name, counts.nrow, xlabel="Modalité", ylabel="Nombre de valeurs", legend=false)
+bar(counts.peak_name, counts.nrow, xlabel="Modalité", ylabel="Nombre de valeurs", legend=true,color=3)
 
 
 
 using StatsPlots
 using DataFrames
-df = dtFinal
-@df df plot(:peak_name,:age)
-@df df scatter(:age,:peak_name,title="My DataFrame Scatter Plot!")
-#variable age
-boxplot(df.age)
-dtFinal=CSV.File(Downloads.download("https://www.dropbox.com/scl/fi/d3v41yp6x9cxlqvueoet3/membersClean.csv?rlkey=v9xfdgu6oyubjur9rlu6k9eow&dl=0")) |> DataFrame
 
-
-
-#partie stat
-counts = combine(groupby(dtFinal, :peak_name), nrow)  #permet d'avoir le nombre de valeurs prises pour chaque modalités
-counts=sort(counts,[order(:nrow)]) #je l'odronne pour que ce soit plus lisible
-using Plots
-bar(counts.peak_name, counts.nrow, xlabel="Modalité", ylabel="Nombre de valeurs", legend=false)
-
-
-
-using StatsPlots
-using DataFrames
 df = copy(dtFinal)
 @df df plot(:peak_name,:age)
 @df df scatter(:age,:peak_name,title="My DataFrame Scatter Plot!")
@@ -53,7 +35,7 @@ bar(classe_age, proportions, xlabel="Catégorie d'âge", ylabel="% Proportion", 
 
 
 
-)
+
 using StatsBase #pour utiliser les tables de frequence
 countsAge = combine(groupby(dtFinal, :age), nrow)  #permet d'avoir le nombre de valeurs prises pour chaque modalités
 classe_age = [0, 20,30,40,50,60,70,80,90]
@@ -98,3 +80,46 @@ title = "Frequency of age groups",label = "Age",xlabel = "age")
 
 histogram(dtFinal.year, bg_inside = "black",title = "Frequency of year",label = "Age", xlabel = "age")
 #on voit une explosion du nb d'exploration à partir des années 80 surement liés aux avancées technologiques
+
+
+
+
+##avec PlotlyJS
+using CSV,Downloads,DataFrames
+dtFinal=CSV.File(Downloads.download("https://www.dropbox.com/scl/fi/d3v41yp6x9cxlqvueoet3/membersClean.csv?rlkey=v9xfdgu6oyubjur9rlu6k9eow&dl=0"),normalizenames = true) |> DataFrame
+
+using PlotlyJS
+
+
+#####histogram#####
+plot(dtFinal,x=:age,kind="histogram")
+
+#####barplot#####
+tab(data ::DataFrame,variable ::Symbol)=combine(groupby(data, variable), nrow)
+x= :peak_name
+y= :sex
+count =tab(dtFinal,x)
+plot(dtFinal,x=count.peak_name,y= count.nrow,kind="bar",Layout(barmode="stack"))
+
+
+#####scatterplot#####
+##classique##
+plot(dtFinal,x=:age,y=:year,kind="scatter",mode="markers")
+
+##selon une variable qualitative##
+plot(dtFinal,x=:age,y=:year,color=:sex,kind="scatter",mode="markers")
+
+#sous population#
+plot(
+    dtFinal, x=:age, y=:year, facet_col=:season,
+    kind="scatter", mode="markers",
+    Layout(
+        width=800, height=800,
+        margin=attr(l=20,r=20,t=20,b=20),
+        paper_bgcolor="LightSteelBlue"
+    )
+)
+
+
+
+
